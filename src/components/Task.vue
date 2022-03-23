2478bb10cf46fa29bcb26963d25276e91cdeb2c2
priscilaalvesfernandes <priscila_eld@hotmail.com455cl>
2022-03-23 12:21:33 -0400

    updt1

---
+++ src/components/Task.vue
@@ -0,0 +1,71 @@
+<template>
+  <div @click="$emit('taskStateChanged', task)"
+        class="task" :class="stateClass">
+      <span @click.stop="$emit('taskDeleted', task)" class="close">X</span><!--STOP nao propaga evento pra outra chamada-->
+      <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: { type: Object, required: true }
+    },
+    computed: {
+        stateClass() {
+            return {
+                pending: this.task.pending,
+                done: !this.task.pending
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .task {
+        position: relative;
+        box-sizing: border-box;
+        width: 350px;
+        height: 150px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor: pointer; /*muda o cursor para a mão*/
+        user-select: none; /*nao permite que o usuário selecione o texto */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .pending {
+        border-left: 12px solid #b73229;
+        background-color: #f44336;
+    }
+
+    .done {
+        color: #ddd;
+        border-left: 12px solid #0a8f08;
+        background-color: #4caf50;
+        text-decoration: line-through;        
+    }
+    .pending .close {
+        background-color: #b73229;
+    }
+    .done .close {
+        background-color: #0a8f08;
+    }
+    .close {
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        font-size: 0.9rem;
+        font-weight: 600;
+        width: 20px;
+        height: 20px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+    }
+</style>
